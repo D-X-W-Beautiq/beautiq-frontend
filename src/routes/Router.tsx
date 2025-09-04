fec2d7cf54a9671d20819d17f3c1712f36e2ff97
HyunisEmpty <empty6004@kookmin.ac.kr>
2025-09-04 17:04:49 +0900

    Feat: Header 컴포넌트 기본 틀 생성

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Apage from '../pages/Apage/Apage'
 import BPage from '../pages/BPage/BPage'
+import TestPage from '../pages/test'
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Apage />} />
         <Route path="/b" element={<BPage />} />
+        <Route path="/t" element={<TestPage />} />
       </Routes>
     </Router>
   )
