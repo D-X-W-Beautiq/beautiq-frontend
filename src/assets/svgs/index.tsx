78847a3576acd68a6c173ae3abfe622a247996d6
sinji <sinzi031012@naver.com>
2025-09-22 22:30:41 +0900

    Feat: 뷰티 팁 박스 퍼블리싱

--- src/assets/svgs/index.tsx
+++ src/assets/svgs/index.tsx
@@ -2,6 +2,7 @@ export { default as IconBack } from "./IconBack";
 export { default as IconBeautiq } from "./IconBeautiq";
 export { default as IconBubbles } from "./IconBubbles";
 export { default as IconClose } from "./IconClose";
+export { default as IconGrowingHeart } from "./IconGrowingHeart";
 export { default as IconHamburger } from "./IconHamburger";
 export { default as IconLipstick } from "./IconLipstick";
 export { default as IconLotionBottle } from "./IconLotionBottle";
