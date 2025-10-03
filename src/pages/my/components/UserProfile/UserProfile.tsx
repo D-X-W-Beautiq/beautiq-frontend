77dfd30b664b00f25929a7c1232738696f92dc54
sinji <sinzi031012@naver.com>
2025-10-03 20:18:27 +0900

    Update: 유저 프로필에 들어올 값 API 명세서와 통일

--- src/pages/my/components/UserProfile/UserProfile.tsx
+++ src/pages/my/components/UserProfile/UserProfile.tsx
@@ -1,5 +1,7 @@
 import * as S from "./UserProfile.styled";
 
+const tempData = { email: "user@example.com", username: "민우", provider: "kakao" };
+
 const UserProfile = () => {
   return (
     <S.UserProfileWrapper>
@@ -8,8 +10,9 @@ const UserProfile = () => {
         alt="프로필 이미지"
       />
       <S.UserInfo>
-        <S.UserName>이름</S.UserName>
-        <S.Text>이메일</S.Text>
+        <S.UserName>{tempData.username}</S.UserName>
+        <S.Text>{tempData.email}</S.Text>
+        {/* TODO : 가입일 백엔드에 요청 후 수정 */}
         <S.Text>가입일</S.Text>
       </S.UserInfo>
     </S.UserProfileWrapper>
